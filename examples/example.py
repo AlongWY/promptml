d56b8f6a04225b9fc91f0dad6cb4434be1193254
ylfeng <ylfeng@ir.hit.edu.cn>
2022-03-13 20:45:18 +0800

    :sparkles: release a base version

--- examples/example.py
+++ examples/example.py
@@ -2,7 +2,7 @@ from promptengine import PromptTemplate
 
 
 def main():
-    res = PromptTemplate("[cls][sent_0][sent_1|a,b,c,d][sent_0] is a [mask][sep]")
+    res = PromptTemplate("[cls]A [mask] news : [sent_0][sep|+]")
     print(res)
     print(res.fragments)
     for p in res.fragments:
